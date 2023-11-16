323ef3f64645399d42ac834344407b650084d853
Bryan Larsen <bryan@larsen.st>
2023-11-16 14:30:01 -0500

    fleek: apply

--- aliases.nix
+++ aliases.nix
@@ -6,5 +6,7 @@
     "latest-fleek-version" = "nix run https://getfleek.dev/latest.tar.gz -- version";
     
     "update-fleek" = "nix run https://getfleek.dev/latest.tar.gz -- update";
+    
+    "windev" = "sudo mount.cifs -o rw,user=bryan,uid=$(id -u),gid=$(id -g),forceuid,forcegid,vers=3.0 //192.168.122.69/bowtie /mnt/windev";
     };
 }
--- user.nix
+++ user.nix
@@ -1,7 +1,7 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
 
-    programs.atuin.enable = true; 
+  #  programs.atuin.enable = true; 
 
     home.sessionVariables = {
 	EDITOR = "vi";
