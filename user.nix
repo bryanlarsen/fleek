665cf309eaaa3e0ed7ed4da2f81247f1b02e6807
Bryan Larsen <bryan@larsen.st>
2023-12-29 18:19:42 -0500

    fleek: apply

--- user.nix
+++ user.nix
@@ -2,6 +2,15 @@
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
 
   #  programs.atuin.enable = true; 
+  programs.bash.initExtra = ''
+    if [ -f "/run/.toolboxenv" ]; then
+        TOOLBOX_NAME=$(cat /run/.containerenv | grep -oP "(?<=name=\")[^\";]+")
+        echo "[${HOSTNAME} ${TOOLBOX_NAME}]"
+	if [[ $TOOLBOX_NAME = rust ]] ; then
+		source /opt/cargo/env
+	fi
+    fi
+  '';
 
     home.sessionVariables = {
 	EDITOR = "vi";
